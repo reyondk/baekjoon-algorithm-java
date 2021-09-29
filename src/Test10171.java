bf931e98fce8faeb7a648236e38578e775dc0387
reyondk <reyondk@gmail.com>
2021-09-29 20:58:44 +0900

    고양이

---
+++ src/Test10171.java
@@ -0,0 +1,21 @@
+/*
+문제
+아래 예제와 같이 고양이를 출력하시오.
+
+입력
+없음.
+
+출력
+고양이를 출력한다.
+*/
+
+public class Test10171 {
+	
+	public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");	
+	}
+
+}
