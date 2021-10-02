c4a7d8713f8f472a39bd3b3edd75a64cc346f0ee
reyondk <reyondk@gmail.com>
2021-10-02 15:34:11 +0900

    파일명 변경

--- src/Test10952.java
+++ src/Test10952.java
@@ -31,7 +31,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Test10592 {
+public class Test10952 {
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
