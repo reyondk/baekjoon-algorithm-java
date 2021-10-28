d495ee895879d3b2c53cd0bcdb60da7d8499e22a
reyondk <reyondk@gmail.com>
2021-10-28 20:07:56 +0900

    웰컴

---
+++ src/Test5337.java
@@ -0,0 +1,23 @@
+/*
+문제
+Welcome을 예제 출력처럼 출력하는 프로그램을 작성하시오.
+
+출력
+Welcome을 아래 예제 출력처럼 출력한다.
+
+예제 입력 1 
+예제 출력 1 
+.  .   .
+|  | _ | _. _ ._ _  _
+|/\|(/.|(_.(_)[ | )(/.
+*/
+
+public class Test5337 {
+	
+	public static void main(String[] args) {
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+	}
+	
+}
