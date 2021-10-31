7d7641db5ff087095b3e9fd6cb187d339a78a611
reyondk <reyondk@gmail.com>
2021-10-31 14:12:01 +0900

    나는 누구인가

---
+++ src/Test15733.java
@@ -0,0 +1,22 @@
+/*
+문제
+당신은 지금 코딩을 하고있다. 그럼 당신은 어떤 사람인가.
+
+입력
+없다.
+
+출력
+“I'm Sexy”를 출력한다.
+
+예제 입력 1 
+예제 출력 1 
+I'm Sexy
+*/
+
+public class Test15733 {
+
+	public static void main(String[] args) {
+		System.out.println("I'm Sexy");
+	}
+	
+}
